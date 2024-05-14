73390dac2365910723089adf20d406888f402ab4
jondoescoding <jondidathing.jm@gmail.com>
2024-05-14 13:05:58 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='housing-sml',
+    version='0.1',
+    packages=['housing-sml'],
+    license='Apache v2',
+    long_description='Serverless Machine Learning on the housing data'
+)
